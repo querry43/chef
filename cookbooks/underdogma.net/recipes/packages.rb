d3853a4a061eb08f46f183d0d1a65615127a26e4
Matt Harrington <querry43@gmail.com>
2018-12-10 07:23:04 +0000

    upgrading terraform, installing go

--- cookbooks/underdogma.net/recipes/packages.rb
+++ cookbooks/underdogma.net/recipes/packages.rb
@@ -2,6 +2,7 @@ package 'apt-transport-https'
 
 packages = [
   'apt-file',
+  'awscli',
   'bc',
   'finger',
   'gcc',
@@ -22,12 +23,29 @@ gem_package 'config_curator'
 gem_package 'ruby-shadow'
 
 archive 'terraform' do
-  url 'https://releases.hashicorp.com/terraform/0.11.10/terraform_0.11.10_linux_amd64.zip'
-  version '0.11.10'
+  url 'https://releases.hashicorp.com/terraform/0.12.0-alpha4/terraform_0.12.0-alpha4_terraform_0.12.0-alpha4_linux_amd64.zip'
+  version '0.12.0-alpha4'
   mode '0755'
   extract_action 'unzip'
 end
 
 link '/usr/local/bin/terraform' do
   to '/usr/local/terraform/versions/current/terraform'
+end
+
+tar_extract 'https://dl.google.com/go/go1.11.2.linux-amd64.tar.gz' do
+  target_dir '/usr/local'
+  creates '/usr/local/go/bin/go'
+end
+
+link '/usr/local/bin/go' do
+  to '/usr/local/go/bin/go'
+end
+
+link '/usr/local/bin/godoc' do
+  to '/usr/local/go/bin/godoc'
+end
+
+link '/usr/local/bin/gofmt' do
+  to '/usr/local/go/bin/gofmt'
 end
